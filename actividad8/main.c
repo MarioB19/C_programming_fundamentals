85d81d937a8c68c42aa655b87250bcd2cc1f164f
Brandon Muro <133941077+MarioB19@users.noreply.github.com>
2024-09-12 10:04:19 -0600

    Adding Activity 8

---
+++ actividad8/main.c
@@ -0,0 +1,49 @@
+/*
+Integrantes
+Mario Brandon Muro Ramos
+Eduardo Roberto Quinio Perez
+
+*/
+
+#include <stdio.h>
+
+
+int km, m, cm;
+int proceso (int);
+
+
+void salida(int , char [20]);
+
+int main() 
+{   
+
+    int input;
+
+    printf("\n Ingrese la cantidad de cm a convertir: ");
+    scanf("%d",&input);
+
+    proceso(input);
+
+    salida(km, "kilometro(s)");
+    salida(m, "metro(s)");
+    salida(cm, "centimetro(s)");
+
+    return 0;
+}
+
+int proceso (int input)
+{
+
+    km = input / 100000;
+
+    m = (input % 100000) /100;
+
+    cm = (input % 100000) % 100;
+
+}
+
+
+void salida(int total, char denominacion [20]){
+    printf("\n %d  %s ", total, denominacion);
+}
+
