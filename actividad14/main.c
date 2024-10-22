f8871b86463b6c6793b28a9520a1c329d80b34b4
Brandon Muro <133941077+MarioB19@users.noreply.github.com>
2024-10-22 09:56:52 -0600

    upload activity 14

---
+++ actividad14/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+
+
+
+    /*
+
+    //Ejercicio 1
+    for(int i =0;i<10;i++){
+        printf("Numero %d : %d\n",i+1,rand()%100);
+    }
+    */
+
+
+    /*
+
+
+    //Ejercicio 2
+
+    int numero;
+
+    srand(time(NULL));
+
+    for(int i =0;i<10;i++){
+        numero =rand()%100;
+        printf("Numero %d : %d %s\n",i+1,numero , numero %2 == 0 ? "par" : "impar");
+    }
+
+    */
+
+
+
+    /*
+    //Ejercicio 3
+
+    int numero;
+    printf("Ingrese un entero: ");
+    scanf("%d",&numero);
+
+
+    printf("Numero ingresado : %d, convertido a: %d ", numero , (numero == 0 ? numero-1 : (numero > 0 ? numero*2 : numero+3)));
+
+    */
+
+
+
+
+    return 0;
+}
